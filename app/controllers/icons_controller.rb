a4fed5186d20be296f2ebab86ebc7ce73bfe16ad
Ben Johnson <benbjohnson@yahoo.com>
2012-02-06 15:07:51 -0700

    Add color option in URL.

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -4,10 +4,8 @@ class IconsController < ApplicationController
     Encoding.default_external = Encoding::BINARY
     
     # Split parameter into the name and id
-    arr = params[:id].split(/-/)
-    icon_index = 0
-    icon_index = arr.pop().to_i if arr.length > 1 && arr.last.index(/^\d+$/)
-    name = arr.join('-')
+    options = parse_options(params[:id])
+    p "OPTIONS: #{options}"
     
     # Setup a place to store the SVG data
     svg_data = nil
@@ -15,13 +13,13 @@ class IconsController < ApplicationController
     
     # Grab the noun's page.
     agent = Mechanize.new()
-    agent.get("http://thenounproject.com/noun/#{name}/") do |page|
+    agent.get("http://thenounproject.com/noun/#{options[:name]}/") do |page|
       # Retrieve a list of all icons available
       items = page.parser.css('ul.thumbnails li')
       
       # Retrieve the item
-      if icon_index < items.length
-        icon_id = items[icon_index]['id'].match(/\d+$/).to_s
+      if options[:index] < items.length
+        icon_id = items[options[:index]]['id'].match(/\d+$/).to_s
         
         # Write zip to temp file
         Tempfile.open(['icon-', '.zip']) do |f|
@@ -31,12 +29,12 @@ class IconsController < ApplicationController
           # Extract SVG from zip file
           ::Zip::ZipFile.open(f.path) do |zipfile|
             zipfile.get_input_stream("noun_project_#{icon_id}.svg") do |zio|
-              svg_data = zio.read
+              svg_data = Nokogiri::XML(zio.read)
             end
           end
         end
       else
-        error_message = "Invalid icon index: #{icon_index} of #{items.length}"
+        error_message = "Invalid icon index: #{options[:index]} of #{items.length}"
       end
     end
     
@@ -46,9 +44,83 @@ class IconsController < ApplicationController
     
     # Otherwise return the SVG
     else
+      # Update the SVG color.
+      if !options[:color].nil?
+        set_svg_fill_color(svg_data, options[:color])
+      end
+
+      # Update the SVG dimension.
+      if !options[:width].nil? && !options[:height].nil?
+        set_svg_size(svg_data, options[:width], options[:height])
+      end
+      
+      # Output SVG.
       if params[:format] == 'svg'
-        render :text => svg_data
+        render :text => svg_data.to_s
       end
     end
   end
+  
+  
+  ##############################################################################
+  # Private Methods
+  ##############################################################################
+  
+  ######################################
+  # Options
+  ######################################
+  
+  # Retrieves a list of options given an id passed to the controller.
+  def parse_options(str)
+    options = {}
+    items = str.split(/-/)
+    
+    # The first option is always the name of the icon.
+    options[:name] = items.shift()
+    options[:name].gsub!(/_/, '-')
+    
+    # The second option is the index if it is a single integer.
+    if !items.first.nil? && !items.first.index(/^\d$/).nil?
+      options[:index] = items.shift().to_i
+    else
+      options[:index] = 0
+    end
+    
+    # The remaining options are determined by format.
+    items.each do |item|
+      # Color: 3 digit hexidecimal number (e.g. fff).
+      if item.index(/^[0-9a-f]{3}$/i)
+        options[:color] = item.downcase.chars.map {|x| x*2}.join('')
+
+      # Color: 6 digit hexidecimal number (e.g. ffffff).
+      elsif item.index(/^[0-9a-f]{6}$/i)
+        options[:color] = item.downcase
+        
+      # Size: Two number joined by an 'x'
+      elsif item.index(/^\d+x\d+$/)
+        width, height = item.split('x').map {|x| x.to_i}
+        options[:width]  = width
+        options[:height] = height
+      end
+    end
+    
+    # Return options
+    return options
+  end
+
+
+  ######################################
+  # SVG
+  ######################################
+  
+  # Changes the fill color of the SVG drawing.
+  def set_svg_fill_color(xml, color)
+    xml.root['style'] = "fill:##{color}"
+  end
+
+  # Changes the size of the SVG drawing.
+  def set_svg_size(xml, width, height)
+    #orig_width = xml.
+  end
+  
 end
